1f10d7b729097d7cf15db9b5ecd1142e328d9dda
Sergey Spisak <serega232003@gmail.com>
2024-06-17 14:44:41 +0300

    'fixes_annotation'

--- src/calc.py
+++ src/calc.py
@@ -1,16 +1,16 @@
-def add(x, y):
+def add(x: int, y: int) -> int:
     return x + y
 
 
-def subtract(x, y):
+def subtract(x: int, y: int) -> int:
     return x - y
 
 
-def multiply(x, y):
+def multiply(x: int, y: int) -> int:
     return x * y
 
 
-def divide(x, y):
+def divide(x: int, y: int) -> float:
     if y == 0:
         raise ZeroDivisionError('Деление на ноль невозможно')
     return x / y
