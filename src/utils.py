e0212b6543164de2fe851c8048798353acf91e76
Sergey Spisak <serega232003@gmail.com>
2024-06-17 13:07:20 +0300

    'add_lessons_test_'

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,11 @@
 def text_reverse(new_string: str) -> str:
     """Переворачивает строку"""
     return new_string[::-1]
+
+
+def up_first(sentence: str) -> str:
+    """Делает первую букву строки заглавной"""
+    if sentence:
+        return sentence[0].upper() + sentence[1:]
+    else:
+        return sentence
