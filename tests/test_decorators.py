8956cf9b5b86063766d32626db4ee7f1f0d4d9a3
Sergey Spisak <serega232003@gmail.com>
2024-06-25 00:49:52 +0300

    fixes tests

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -18,3 +18,12 @@ def test_log_without_filename(capsys):
     my_function(4, 4)
     captured = capsys.readouterr()
     assert "my_function ok. Result: 8\n" in captured.out
+
+
+def test_log_if_error():
+    @log()
+    def my_function(a, b):
+        return a + b
+
+    my_function(1, 'f')
+    assert "my_function error: unsupported operand type(s) for +: 'int' and 'str'. Inputs: (1, 'f'), {}"
