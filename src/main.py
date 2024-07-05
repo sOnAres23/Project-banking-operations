7e0b211467a3e12f11f452c14c27bf07c83e7107
Sergey Spisak <serega232003@gmail.com>
2024-07-05 20:09:03 +0300

    add_for_homework_12_1

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
-from src.utils import text_reverse
+def text_reverse(new_string: str) -> str:
+    """Переворачивает строку"""
+    return new_string[::-1]
+
 
 if __name__ == '__main__':
     print(text_reverse('Hello, guys!'))
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,18 @@
-def text_reverse(new_string: str) -> str:
-    """Переворачивает строку"""
-    return new_string[::-1]
+import json
+from typing import Any, Dict, List
+
+
+def read_transactions_from_json(json_file_path: str) -> List[Dict[str, Any]]:
+    """Функция считывающая информацию JSON формата с заданного файла"""
+    try:
+        with open(json_file_path, encoding='utf-8') as file:
+            data = json.load(file)
+            if isinstance(data, list):
+                print(data)
+            else:
+                return []
+    except FileNotFoundError:
+        return []
+
+
+read_transactions_from_json('../data/operations.json')
